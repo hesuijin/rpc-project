ffa10db4792f06fb905898a0b28a35246f6f90b5
HSJ <hesuijin@qq.com>
2021-01-07 00:49:06 +0800

    引入包 kryo

---
+++ src/main/java/com/example/rpc/demo/MyTest.java
@@ -0,0 +1,15 @@
+package com.example.rpc.demo;
+
+/**
+ * @Author HSJ
+ * @Date 2021/1/7 0:32
+ * @Description:
+ */
+public class MyTest {
+
+
+    public String demo(String myName){
+     return "Hello World" + myName;
+    }
+
+}
