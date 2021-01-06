4c5e595e7c67d5f522c6c75abcf37942c5171754
HSJ <hesuijin@qq.com>
2021-01-07 00:18:29 +0800

    创建rpc新项目

---
+++ src/main/java/com/example/rpc/RpcProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.rpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpcProjectApplication.class, args);
+    }
+
+}
