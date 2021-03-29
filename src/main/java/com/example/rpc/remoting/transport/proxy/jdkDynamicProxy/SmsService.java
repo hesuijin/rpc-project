5319a3079d0887ec0f252dbd5615368ec6e7c960
HSJ <hesuijin@qq.com>
2021-03-29 21:29:07 +0800

    实现JDK动态代理

---
+++ src/main/java/com/example/rpc/remoting/transport/proxy/jdkDynamicProxy/SmsService.java
@@ -0,0 +1,12 @@
+package com.example.rpc.remoting.transport.proxy.jdkDynamicProxy;
+
+/**
+ * @Description:
+ * @Author HeSuiJin
+ * @Date 2021/3/29
+ */
+public interface SmsService {
+
+    String send(String message);
+
+}
