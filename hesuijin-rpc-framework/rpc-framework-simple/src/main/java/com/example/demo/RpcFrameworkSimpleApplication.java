b82c5ad58abf38ade6296de9d22a70bcbda5f3f8
HSJ <hesuijin@qq.com>
2021-04-02 11:37:07 +0800

    新增 rpc-framework模块

---
+++ hesuijin-rpc-framework/rpc-framework-simple/src/main/java/com/example/demo/RpcFrameworkSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcFrameworkSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpcFrameworkSimpleApplication.class, args);
+    }
+
+}
