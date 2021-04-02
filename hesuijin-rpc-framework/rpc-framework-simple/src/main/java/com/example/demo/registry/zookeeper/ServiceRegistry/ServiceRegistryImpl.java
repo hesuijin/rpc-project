ab11996f6d5af5e6251ab20b325eea2c3e63e030
HSJ <hesuijin@qq.com>
2021-04-02 13:01:24 +0800

    RPC框架  新增common(公共包服务)

--- hesuijin-rpc-framework/rpc-framework-simple/src/main/java/com/example/demo/registry/zookeeper/ServiceRegistry/ServiceRegistryImpl.java
+++ hesuijin-rpc-framework/rpc-framework-simple/src/main/java/com/example/demo/registry/zookeeper/ServiceRegistry/ServiceRegistryImpl.java
@@ -1,15 +1,26 @@
 package com.example.demo.registry.zookeeper.ServiceRegistry;
 
+import com.example.demo.registry.zookeeper.CuratorUtils;
+import org.apache.curator.framework.CuratorFramework;
+
 import java.net.InetSocketAddress;
 
 /**
  * @Description:
+ * 创建持久化节点
  * @Author HeSuiJin
  * @Date 2021/4/2
  */
 public class ServiceRegistryImpl implements ServiceRegistry {
+
+    /**用于创建持久化节点
+     * @param rpcServiceName
+     * @param inetSocketAddress
+     */
     @Override
     public void registerService(String rpcServiceName, InetSocketAddress inetSocketAddress) {
-
+        String servicePath = CuratorUtils.ZK_REGISTER_ROOT_PATH + "/" + rpcServiceName + inetSocketAddress.toString();
+//        CuratorFramework zkClient = CuratorUtils.getZkClient();
+//        CuratorUtils.createPersistentNode(zkClient, servicePath);
     }
 }
