cd5d6bc247ae869f2d74ff8c53b7d96be5b961b8
HSJ <hesuijin@qq.com>
2021-04-02 13:03:27 +0800

    RPC框架  修改ServiceRegistry

--- hesuijin-rpc-framework/rpc-framework-simple/src/main/java/com/example/demo/registry/zookeeper/ServiceRegistry/ServiceRegistryImpl.java
+++ hesuijin-rpc-framework/rpc-framework-simple/src/main/java/com/example/demo/registry/zookeeper/ServiceRegistry/ServiceRegistryImpl.java
@@ -19,8 +19,5 @@ public class ServiceRegistryImpl implements ServiceRegistry {
      */
     @Override
     public void registerService(String rpcServiceName, InetSocketAddress inetSocketAddress) {
-        String servicePath = CuratorUtils.ZK_REGISTER_ROOT_PATH + "/" + rpcServiceName + inetSocketAddress.toString();
-//        CuratorFramework zkClient = CuratorUtils.getZkClient();
-//        CuratorUtils.createPersistentNode(zkClient, servicePath);
     }
 }
