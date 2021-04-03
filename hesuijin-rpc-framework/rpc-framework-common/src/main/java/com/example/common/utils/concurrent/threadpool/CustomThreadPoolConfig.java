d85fb88b8b8a18e387cbde348719e6ce297950e8
HSJ <hesuijin@qq.com>
2021-04-03 23:46:59 +0800

    设置线程池 核心参数常量

---
+++ hesuijin-rpc-framework/rpc-framework-common/src/main/java/com/example/common/utils/concurrent/threadpool/CustomThreadPoolConfig.java
@@ -0,0 +1,39 @@
+package com.example.common.utils.concurrent.threadpool;
+
+import lombok.Getter;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 线程池的7个核心参数   除了生成工厂 和 拒绝策略  其他在这这里设置
+ * @Description:线程池常量
+ * @Author HeSuiJin
+ * @Date 2021/4/3
+ */
+@Getter
+public class CustomThreadPoolConfig {
+
+    /**
+     * 线程池默认参数
+     */
+    //线程池
+
+    private static final int DEFAULT_CORE_POOL_SIZE = 10;
+    private static final int DEFAULT_MAXIMUM_POOL_SIZE_SIZE = 100;
+    private static final int DEFAULT_KEEP_ALIVE_TIME = 1;
+    private static final TimeUnit DEFAULT_TIME_UNIT = TimeUnit.MINUTES;
+
+    private static final int DEFAULT_BLOCKING_QUEUE_CAPACITY = 100;
+//    private static final int BLOCKING_QUEUE_CAPACITY = 100;
+    /**
+     * 可配置参数
+     */
+    private int corePoolSize = DEFAULT_CORE_POOL_SIZE;
+    private int maximumPoolSize = DEFAULT_MAXIMUM_POOL_SIZE_SIZE;
+    private long keepAliveTime = DEFAULT_KEEP_ALIVE_TIME;
+    private TimeUnit unit = DEFAULT_TIME_UNIT;
+    // 使用有界队列
+    private BlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<>(DEFAULT_BLOCKING_QUEUE_CAPACITY);
+}
