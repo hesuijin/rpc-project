6be0d852b88b97cda11340bfe50bede7d9ea4b52
HSJ <hesuijin@qq.com>
2023-03-25 12:06:26 +0800

    提交socketdemo代码

--- rpc-project-demo/src/main/java/com/example/demo/socketDemo/SocketHelloServer.java
+++ rpc-project-demo/src/main/java/com/example/demo/socketDemo/SocketHelloServer.java
@@ -16,6 +16,19 @@ import java.net.Socket;
 @Slf4j
 public class SocketHelloServer {
 
+//    try-with-resource语句:
+//        try括号中包含资源声明了一种或多种资源，并且保证每个声明了的资源在语句结束时都会被自动关闭，
+//        那么如何定义资源，任何实现了java.lang.AutoCloseable（Closeable）接口的对象，都可以在try-with-resource语句中使用，
+//        最终在语句结束时会把资源全部关闭掉。
+//
+//        如： try (Socket socket = new Socket(host, port))   中最后会自动执行 socket.close();
+//        输出流 输入流也是类似：
+//        objectOutputStream.close();  objectInputStream.close();
+//
+//    objectOutputStream.flush():
+//        部分outputstream的子类实现了缓存机制，为了提高效率,当write()的时候不一定直接发过去，有可能先缓存起来一起发出去,
+//        flush()的作用就是强制性地将缓存中的数据发出去.
+
     public void start(int port) {
         //1.创建 ServerSocket 对象并且绑定一个端口  当结束后 会关闭ServerSocket
         try (ServerSocket server = new ServerSocket(port);) {
