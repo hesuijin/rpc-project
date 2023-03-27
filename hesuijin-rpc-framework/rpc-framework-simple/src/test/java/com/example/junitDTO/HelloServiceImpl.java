2c483a35cc26374323e82618da13b74440074cb7
HSJ <hesuijin@qq.com>
2023-03-27 19:05:15 +0800

    单元测试格式化

--- hesuijin-rpc-framework/rpc-framework-simple/src/test/java/com/example/junitDTO/HelloServiceImpl.java
+++ hesuijin-rpc-framework/rpc-framework-simple/src/test/java/com/example/junitDTO/HelloServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.junitDTO;
 
-import com.example.demo.annotation.RpcService;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -9,18 +8,17 @@ import lombok.extern.slf4j.Slf4j;
  * @Date 2021/4/5
  */
 @Slf4j
-@RpcService(group = "socketSeverNameGroup", version = "socketSeverNameVersion")
 public class HelloServiceImpl implements HelloService {
 
     static {
-        System.out.println("HelloServiceImpl2 被创建");
+        System.out.println("HelloServiceImpl 被创建");
     }
 
     @Override
     public String helloServiceApi(Hello hello) {
-        log.info("HelloServiceImpl2 收到: {}.", hello.getMessage());
+        log.info("HelloServiceImpl 收到: {}.", hello.getMessage());
         String result = "Hello description is " + hello.getDescription();
-        log.info("HelloServiceImpl2 返回: {}.", result);
+        log.info("HelloServiceImpl 返回: {}.", result);
         return result;
     }
 
